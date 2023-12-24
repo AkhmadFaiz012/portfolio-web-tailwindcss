ba5390c83d859d982e7b01b6271219cab387be8b
AkhmadFaiz012 <akhmadf109@gmail.com>
2023-12-24 21:41:57 +0700

    1.0.0

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary : '#2563EB',
+        secondary : '#F97316',
+        accent : '#DC2626',
+        white : '#FAFAFA',
+        dark : '#0A0A0A'
+      },
+      fontfamily:{
+        sans : 'system-ui'
+      },
+      container : {
+        center : true,
+        padding : '1rem',
+        screens : {
+          sm : '600px',
+          '2xl': '1124px'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
